83c8281d8c57a6438b7a82b7c46d3271dcccdde9
belikeberlin <115548163+belikeberlin@users.noreply.github.com>
2022-10-11 17:58:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Saifudeebot/Berlinbrofilter.git /EvaMaria
+  git clone https://github.com/belikeberlin/Sm18filterbot.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
